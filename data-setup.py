2d75dc5d136e74541a246e90449b69fee4e99677
Naman Arora <namanarora@cmu.edu>
2022-02-27 20:35:00 -0500

    added data and initial script

---
+++ data-setup.py
@@ -0,0 +1,40 @@
+import pandas as pd
+path = './datasets/wid-all/'
+
+print('Reading US data...')
+
+us = pd.read_csv(path+'WID_data_US.csv', sep = ';')
+
+print('Done reading US data')
+print('Reading US metadata...')
+
+us_meta = pd.read_csv(
+    path+'WID_metadata_US.csv', 
+    sep = ';',
+    encoding='ISO-8859-1'
+)
+
+print('Data Loaded')
+
+# Fixing the error
+us_meta['variable'] = us_meta['variable'].apply(
+    lambda x : x[:-4] + x[-3:] + x[-4]
+)
+
+print('Merging...')
+
+us = us.merge(
+    us_meta[['variable', 'shortname', 'shortage', 'simpledes',
+             'shorttype', 'shortpop', 'unit']],
+    on = 'variable',
+    how = 'inner'
+).drop(
+    ['age', 'pop'],
+    axis = 1
+)
+
+print('Writing us-master.csv...')
+
+us.to_csv(path+'us-master.csv')
+
+print('Done')
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,30 +2,60 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
-st.title("Let's analyze some Penguin Data 🐧📊.")
+st.title("Where do the $$$ go?")
 
 @st.cache  # add caching so we load the data only once
 def load_data():
-    # Load the penguin data from https://github.com/allisonhorst/palmerpenguins.
-    penguins_url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/v0.1.0/inst/extdata/penguins.csv"
-    return pd.read_csv(penguins_url)
+    '''
+    Load the processed master data set
+    '''
+    master_url = './datasets/wid-all/us-master.csv'
+    return pd.read_csv(master_url)
 
-df = load_data()
+@st.cache # caching for faster access
+def load_meta():
+    '''
+    Load the metadata dataset 
+    '''
+    meta_url = './datasets/wid-all/WID_metadata_US.csv'
+    return pd.read_csv(
+    meta_url, 
+    sep = ';',
+    encoding='ISO-8859-1'
+    )
 
-st.write("Let's look at raw data in the Pandas Data Frame.")
+# Load the data
+with st.spinner(text="Loading data..."):
+    us = load_data()
+    meta = load_meta()
 
-st.write(df)
+# Display data if user wants 
+if st.checkbox('Show data'):
+    st.write(us[:20])    
+    st.write('Only first 20 rows were loaded.')
+    if st.checkbox('Show entire data (may be slow)'):
+        slice_age = st.slider(
+            'Select a range of indices', 
+            0, len(us), 
+            (0, 50)
+        )
+        min_range, max_range = slice_age
+        st.write(us[min_range:max_range+1])
 
-st.write("Hmm 🤔, is there some correlation between body mass and flipper length? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
 
-chart = alt.Chart(df).mark_point().encode(
-    x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
-    y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
-    color=alt.Y("species")
-).properties(
-    width=600, height=400
-).interactive()
 
-st.write(chart)
 
-st.markdown("This project was created by Student1 and Student2 for the [Interactive Data Science](https://dig.cmu.edu/ids2022) course at [Carnegie Mellon University](https://www.cmu.edu).")
+
+# st.write("Hmm 🤔, is there some correlation between body mass and flipper length? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
+
+# chart = alt.Chart(df).mark_point().encode(
+#     x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
+#     y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
+#     color=alt.Y("species")
+# ).properties(
+#     width=600, height=400
+# ).interactive()
+
+# st.write(chart)
+
+st.markdown("This project was created by [Naman](mailto:namanarora@cmu.edu) and [Nate](ndf@andrew.cmu.edu) for the [Interactive Data Science](https://dig.cmu.edu/ids2022) course at [Carnegie Mellon University](https://www.cmu.edu).")
